05323cd6ed566a893e61cb49b4d61288fe0e8b43
jcompaire <themasterserver@protonmail.ch>
2026-05-06 09:54:03 -0300

    Adding README.py file

---
+++ README.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# Python
+A Python's toolbox to deal with big data processing and statistical analysis in marine sciences
+
+## Python Projects
+
+- [download CMEMS data](https://github.com/jcompaire/download_CMEMS_data)
+- [Notorynchus cepedianus growth analyses](https://github.com/jcompaire/FishGrowth_Ncepedianus)
+"""
+
+
